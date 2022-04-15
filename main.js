bd1918dfc24ef171743eeed85f2743598e21a81a
Anthony Romero-Jorge <AnthonyRomero@MacBook-Pro.local>
2022-04-15 13:55:49 -0400

    uploaded js and html files

---
+++ main.js
@@ -0,0 +1,14 @@
+//Question 1
+
+alert("Deposit Money");
+
+//Question 2
+let money = 525;
+let totalMoney = 1000 + money;
+let moneyAmount = prompt("How much would you like to deposit?");
+if (money === 525) {
+    alert(`Your new balance is ${totalMoney}.`);
+};
+
+//Question 3
+alert("You got monnneeeyy!!");
